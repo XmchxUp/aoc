9f9b3a6a01d7a88e27f81639b51107f26e331807
Tesla <1394466835@qq.com>
2024-12-02 15:59:18 +0800

    update

---
+++ aoc2024/src/day2/mod.rs
@@ -0,0 +1,80 @@
+use aoclib::Runner;
+
+#[derive(Default)]
+pub struct Aoc2023_2 {
+    levels: Vec<Vec<i32>>,
+}
+
+impl Aoc2023_2 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    fn is_safe_report(&self, nums: &[i32]) -> bool {
+        let asc = nums[1] > nums[0];
+        for i in 1..nums.len() {
+            let diff = nums[i] - nums[i - 1];
+            if diff == 0 || diff.abs() > 3 || (diff > 0) != asc {
+                return false;
+            }
+        }
+        true
+    }
+    fn is_safe_report_by_remove_one(&self, nums: &[i32]) -> bool {
+        for i in 0..nums.len() {
+            let mut modified = nums.to_vec();
+            modified.remove(i);
+            if self.is_safe_report(&modified) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+impl Runner for Aoc2023_2 {
+    fn info(&self) -> (usize, usize) {
+        (2024, 2)
+    }
+
+    fn parse(&mut self) {
+        let inputs = aoclib::utils::read_file("./inputs/input02.txt");
+        inputs.iter().for_each(|line| {
+            self.levels.push(
+                line.split(' ')
+                    .into_iter()
+                    .map(|v| v.parse::<i32>().unwrap())
+                    .collect(),
+            );
+        });
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        vec![format!(
+            "{}",
+            self.levels
+                .iter()
+                .filter_map(|nums| if self.is_safe_report(nums) {
+                    Some(1)
+                } else {
+                    Some(0)
+                })
+                .sum::<i32>()
+        )]
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        vec![format!(
+            "{}",
+            self.levels
+                .iter()
+                .filter_map(|nums| {
+                    if self.is_safe_report(nums) || self.is_safe_report_by_remove_one(nums) {
+                        Some(1)
+                    } else {
+                        Some(0)
+                    }
+                })
+                .sum::<i32>()
+        )]
+    }
+}
--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -4,14 +4,17 @@ use aoclib::{Runner, Selector};
 
 mod day1;
 use day1::*;
-
+mod day2;
+use day2::*;
 fn main() {
     let day1 = Aoc2023_1::new();
+    let day2 = Aoc2023_2::new();
 
     let mut days: HashMap<usize, Box<dyn Runner>> = HashMap::new();
     days.insert(1, Box::new(day1));
+    days.insert(2, Box::new(day2));
 
-    let which = Selector::One(1);
+    let which = Selector::One(2);
 
     match which {
         Selector::All => todo!(),
