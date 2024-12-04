ed01c88a5b928d47c2bedd5f096c7f245f35166f
Tesla <1394466835@qq.com>
2024-12-04 15:26:31 +0800

    update

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -8,18 +8,22 @@ mod day2;
 use day2::*;
 mod day3;
 use day3::*;
+mod day4;
+use day4::*;
 
 fn main() {
     let day1 = Aoc2024_1::new();
     let day2 = Aoc2024_2::new();
     let day3 = Aoc2024_3::new();
+    let day4 = Aoc2024_4::new();
 
     let mut days: HashMap<usize, Box<dyn Runner>> = HashMap::new();
     days.insert(1, Box::new(day1));
     days.insert(2, Box::new(day2));
     days.insert(3, Box::new(day3));
+    days.insert(4, Box::new(day4));
 
-    let which = Selector::One(3);
+    let which = Selector::One(4);
 
     match which {
         Selector::All => todo!(),
