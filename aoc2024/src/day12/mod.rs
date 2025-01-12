df40daf9db4fb9c9af27756ea2d332fc03e7dc40
ultraman <1394466835@qq.com>
2025-01-12 16:49:55 +0800

    update

--- aoc2024/src/day12/mod.rs
+++ aoc2024/src/day12/mod.rs
@@ -2,39 +2,26 @@ use std::collections::{HashSet, VecDeque};
 
 use aoclib::Runner;
 
-#[derive(Default)]
 pub struct Aoc2024_12 {
     map: Vec<Vec<char>>,
+    dirs: Vec<(i32, i32)>,
+    regions: Vec<HashSet<(i32, i32)>>,
 }
 
 impl Aoc2024_12 {
     pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Runner for Aoc2024_12 {
-    fn info(&self) -> (usize, usize) {
-        (2024, 12)
-    }
-
-    fn parse(&mut self) {
-        // let inputs =
-        // aoclib::utils::read_file(format!("./inputs/test{:02}.txt", self.info().1).as_str());
-        let inputs =
-            aoclib::utils::read_file(format!("./inputs/input{:02}.txt", self.info().1).as_str());
-
-        for line in inputs {
-            self.map.push(line.chars().collect());
+        Self {
+            map: Vec::new(),
+            dirs: vec![(0, 1), (0, -1), (1, 0), (-1, 0)],
+            regions: Vec::new(),
         }
     }
 
-    fn part1(&mut self) -> Vec<String> {
+    fn parse_region(&mut self) {
         let mut regions = Vec::new();
-        let dirs = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
 
-        let m = self.map.len();
-        let n = self.map[0].len();
+        let m = self.map.len() as i32;
+        let n = self.map[0].len() as i32;
 
         let mut seen = HashSet::new();
         for i in 0..m {
@@ -53,50 +40,126 @@ impl Runner for Aoc2024_12 {
                 while !q.is_empty() {
                     let (ii, jj) = q.pop_front().unwrap();
 
-                    for dir in &dirs {
-                        let ni = ii as i32 + dir.0;
-                        let nj = jj as i32 + dir.1;
-                        if ni < 0 || nj < 0 || ni >= m as i32 || nj >= n as i32 {
+                    for dir in &self.dirs {
+                        let ni = ii + dir.0;
+                        let nj = jj + dir.1;
+                        if ni < 0 || nj < 0 || ni >= m || nj >= n {
                             continue;
                         }
-                        if self.map[ni as usize][nj as usize] != self.map[i][j] {
+                        if self.map[ni as usize][nj as usize] != self.map[i as usize][j as usize] {
                             continue;
                         }
-                        if region.contains(&(ni as usize, nj as usize)) {
+                        if region.contains(&(ni, nj)) {
                             continue;
                         }
-                        region.insert((ni as usize, nj as usize));
-                        q.push_back((ni as usize, nj as usize));
+                        region.insert((ni, nj));
+                        q.push_back((ni, nj));
                     }
                 }
-                let _ = seen.union(&region);
+                seen.extend(region.iter());
                 regions.push(region);
             }
         }
+        self.regions = regions;
+    }
+}
 
-        let perimeter = |region: &HashSet<(usize, usize)>| -> usize {
+impl Runner for Aoc2024_12 {
+    fn info(&self) -> (usize, usize) {
+        (2024, 12)
+    }
+
+    fn parse(&mut self) {
+        // let inputs =
+        // aoclib::utils::read_file(format!("./inputs/test{:02}.txt", self.info().1).as_str());
+        let inputs =
+            aoclib::utils::read_file(format!("./inputs/input{:02}.txt", self.info().1).as_str());
+
+        for line in inputs {
+            self.map.push(line.chars().collect());
+        }
+        self.parse_region();
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        let m = self.map.len() as i32;
+        let n = self.map[0].len() as i32;
+        let price = |region: &HashSet<(i32, i32)>| -> usize {
             let mut res = 0;
             for (i, j) in region {
                 res += 4;
-                for dir in &dirs {
-                    let ni = *i as i32 + dir.0;
-                    let nj = *j as i32 + dir.1;
-                    if ni < 0 || nj < 0 || ni >= m as i32 || nj >= n as i32 {
+                for dir in &self.dirs {
+                    let ni = *i + dir.0;
+                    let nj = *j + dir.1;
+                    if ni < 0 || nj < 0 || ni >= m || nj >= n {
                         continue;
                     }
-                    if region.contains(&(ni as usize, nj as usize)) {
+                    if region.contains(&(ni, nj)) {
                         res -= 1;
                     }
                 }
             }
-            res
+            res * region.len()
         };
 
-        vec![format!("{}", regions.iter().map(perimeter).sum::<usize>())]
+        vec![format!("{}", self.regions.iter().map(price).sum::<usize>())]
     }
 
     fn part2(&mut self) -> Vec<String> {
-        let res = 0;
-        vec![format!("{}", res)]
+        let price = |region: &HashSet<(i32, i32)>| -> usize {
+            let mut corner_candidates = HashSet::new();
+
+            for (i, j) in region {
+                let fi = *i as f32;
+                let fj = *j as f32;
+                for (ii, jj) in [
+                    (fi - 0.5, fj - 0.5),
+                    (fi + 0.5, fj - 0.5),
+                    (fi + 0.5, fj + 0.5),
+                    (fi - 0.5, fj + 0.5),
+                ] {
+                    let scaled_ii = (ii * 100.0).round() as i32;
+                    let scaled_jj = (jj * 100.0).round() as i32;
+                    corner_candidates.insert((scaled_ii, scaled_jj));
+                }
+            }
+
+            let mut corners = 0;
+
+            for (i, j) in corner_candidates {
+                let mut flags = vec![];
+                for (ii, jj) in [
+                    (i - 50, j - 50),
+                    (i + 50, j - 50),
+                    (i + 50, j + 50),
+                    (i - 50, j + 50),
+                ] {
+                    flags.push(region.contains(&(ii / 100, jj / 100)));
+                }
+
+                let number: i32 = flags.iter().map(|f| if *f { 1 } else { 0 }).sum();
+                match number {
+                    1 => corners += 1,
+                    3 => corners += 1,
+                    2 => {
+                        if flags == [true, false, true, false]
+                            || flags == [false, true, false, true]
+                        {
+                            corners += 2;
+                        }
+                    }
+                    4 => corners += 0,
+                    _ => {
+                        panic!("error number:{}", number)
+                    }
+                }
+            }
+
+            // println!("{} * {}", corners, region.len());
+            corners * region.len()
+        };
+
+        // println!("{:?}", self.regions);
+        vec![format!("{}", self.regions.iter().map(price).sum::<usize>())]
     }
 }
