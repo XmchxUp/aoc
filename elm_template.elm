c54f4d2799cc467930b824296ac7137a3615b776
ultraman <sunhuayangak47@gmail.com>
2025-06-07 10:27:23 +0800

    update

---
+++ elm_template.elm
@@ -0,0 +1,27 @@
+module Day2 exposing (..)
+
+import Html exposing (text)
+
+
+inputs =
+    ""
+
+
+main =
+    text ("Part1: " ++ String.fromInt part1 ++ "Part2: " ++ String.fromInt part2)
+
+
+part1 =
+    part1_helper inputs
+
+
+part1_helper input =
+    0
+
+
+part2 =
+    part2_helper inputs
+
+
+part2_helper input =
+    0
